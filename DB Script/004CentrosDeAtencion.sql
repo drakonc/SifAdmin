5e31658f673a7f40a25f2c7e514ac6e8dc626a00
Jose Castro <drakonc@gmail.com>
2019-06-27 18:11:24 -0500

    .

--- DB Script/004CentrosDeAtencion.sql
+++ DB Script/004CentrosDeAtencion.sql
@@ -1,10 +1,10 @@
 USE sifadmin;
 
 CREATE TABLE Centros_de_Atencion(
-	Codigo _Centro INT NOT NULL AUTO_INCREMENT,
+	Codigo_Centro INT NOT NULL AUTO_INCREMENT,
 	Nombre_Centro VARCHAR(50) NULL,
 	Hospitaliza TINYINT(3) UNSIGNED NULL,
-	IPS TINYINT(3) UNSIGNED]NULL,
-	PRIMARY KEY (`Codigo _Centro`),
+	IPS TINYINT(3) UNSIGNED NULL,
+	PRIMARY KEY (`Codigo_Centro`),
 	FOREIGN KEY (IPS) REFERENCES Ips(Control) 
 );
