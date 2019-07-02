d0d7236e4663f8d31fb6e2d3b3d97d60bf0b2b32
Jose Castro <drakonc@users.noreply.github.com>
2019-07-02 18:42:03 -0500

    Creacion de Area E intalacion de DotNet-CodeGenerator

---
+++ SifAdmin/Areas/Departamento/Controllers/DepartamentoController.cs
@@ -0,0 +1,95 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SifAdmin.Areas.Departamento.Models;
+
+namespace SifAdmin.Areas.Departamento.Controllers
+{
+    [Area("Departamento")]
+    public class DepartamentoController : Controller
+    {
+        // GET: Departamento
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Departamento/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Departamento/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Departamento/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Departamento/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Departamento/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Departamento/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Departamento/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ SifAdmin/Areas/Departamento/Models/Departamento.cs
@@ -0,0 +1,9 @@
+using System;
+
+namespace SifAdmin.Areas.Departamento.Models
+{
+    public class Departamento
+    {
+
+    }
+}
